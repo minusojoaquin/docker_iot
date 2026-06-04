d64911b06b715d31d430ec435043f1f44d94d7ef
Joaquin Minuso <joaquinjavierminuso@gmail.com>
2026-06-04 15:47:29 -0300

    Cambios

--- crud/agenda.sql
+++ crud/agenda.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE `agenda` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `agenda`;
 CREATE TABLE `agenda`.`contactos` (`id` UUID NOT NULL DEFAULT UUID_V7() PRIMARY KEY, `nombre` VARCHAR(100) NULL , `tel` VARCHAR(50) NULL , `email` VARCHAR(50) NULL ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-CREATE USER 'crud'@'%'  IDENTIFIED BY 'cambiarcambiar';
+CREATE USER 'crud'@'%'  IDENTIFIED BY 'passworddeiot';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `agenda`.* TO 'crud'@'%';
